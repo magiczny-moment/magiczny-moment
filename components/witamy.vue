0ce5e7168a39d6bf1f80298515def5f88e95a8c5
mrGrochowski <mateusz.grochowski1992@gmail.com>
2025-01-11 20:38:23 +0100

    change vitamy to autoplay video

--- components/witamy.vue
+++ components/witamy.vue
@@ -1,8 +1,10 @@
 <template>
   <figure class="relative h-section md:!h-screen overflow-hidden bg-gray-800 bg-cover">
-    <img :src="image"
-      class="absolute top-0 left-0 object-cover object-center w-screen h-screen full-width smooth-slide" />
-
+    <img :src="image" class="absolute top-0 left-0 object-cover object-center w-screen h-screen" />
+    <video autoplay muted loop class="absolute top-0 left-0 object-cover object-center w-screen h-screen">
+      <source src="/public/filmik_glowna.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
     <!-- Background Image -->
 
 
@@ -10,7 +12,7 @@
     <div class="relative z-10 flex items-center justify-center h-full px-4 text-center text-white md:pb-0">
       <div>
         <img src="/public/LOGO.png" alt="Logo" class="size-[400px] mx-auto mb-4 object-contain" />
-        <div class="max-w-xl mx-auto mb-8 text-lg md:text-xl">
+        <div v-if="content?.length > 0" class="max-w-xl mx-auto mb-8 text-lg md:text-xl">
           {{ content }}
         </div>
         <a :href="`#${link}`" class="px-6 py-3 font-semibold text-white rounded-full bg-accent hover:bg-accent-600">
