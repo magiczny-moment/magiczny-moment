e4dbdd50836d842b66852c36d0ece439f9e8b462
mrGrochowski <mateusz.grochowski1992@gmail.com>
2025-04-19 16:32:09 +0200

    add changes in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,24 @@
 <template>
-  <main class="content-grid">
-    <Navigation v-if="!isAnimationAnywhere" />
-    <template v-for="(section, index) in sections" :key="section._id">
-      <template v-if="section._stem.indexOf('witamy') > -1">
-        <Witamy class="full-width" :id="useGenHumanReadableId(section._stem)" v-bind="section.fields"
-          :link="useGenHumanReadableId(sections[index + 1]._stem)" />
-      </template>
-      <template v-else>
-        <GenerycznaSekcja class="full-width"
-          :class="{ 'mb-[--menu-height]': index + 1 === lengthOfSections && !isAnimationAnywhere }"
-          :id="useGenHumanReadableId(section._stem)" :isAnimation="isAnimation[index]" v-bind="section.fields"
-          @preview-clicked="previewClicked(index)" />
-      </template>
-
-      <!-- <ContentRendererMarkdown :value="section.body" /> -->
-    </template>
+  <main class="bg-gray-900 content-grid text-accent">
+    <Navigation :sections="sections" />
+    <Witamy class="full-width" id="witamy" v-bind="witamy.fields" link="oferta" />
+    <Offer :offers="offers" id="oferta" />
+    <Contact id="kontakt" class="full-width" />
   </main>
 </template>
 
 <script setup lang="ts">
-import { useGenHumanReadableId } from '../composable/useGenHumanReadableId'
+const sections = ref([
+  { id: 'witamy', title: 'Witamy' },
+  { id: 'oferta', title: 'Oferta' },
+  { id: 'media', title: 'Media społecznościowe' },
+  { id: 'kontakt', title: 'Kontakt' },
+])
 
-const { data: sections } = await useAsyncData('sections', async () => {
-  return await queryContent().find()
+const { data: offers } = await useAsyncData('offers', async () => {
+  return await queryContent('offers').find()
 })
-
-const lengthOfSections = computed(() => toRaw(sections.value).length).value
-
-const isAnimation = ref([])
-isAnimation.value = Array(lengthOfSections).fill(false)
-const previewClicked = (index: number) => {
-  isAnimation.value = isAnimation.value.map((elem, i) => index === i ? !elem : elem)
-}
-const isAnimationAnywhere = ref(false);
-
-watch(isAnimation, (newIsAnimation, oldIsAnimation) => {
-  isAnimationAnywhere.value = newIsAnimation.some((elem) => elem)
-})
+const { data: witamy } = await useAsyncData('witamy', async () => {
+  return await queryContent().where({ title: { $contains: 'Witamy' } }).findOne();
+});
 </script>
-
-<style lang="css">
-.cos {}
-</style>
--- pages/oferta.vue
+++ pages/oferta.vue
@@ -1,28 +1,50 @@
 <template>
-  <main class="bg-gray-900 content-grid text-accent">
-    <Navigation />
-    <div class="flex flex-col justify-end w-full py-4 md:justify-center h-section">
-      <h1 class="mb-auto text-5xl md:mb-4">Oferta</h1>
-      <div>
-        <div class="overflow-hidden" ref="emblaRef">
-          <div
-            class="grid auto-cols-[80%] md:auto-cols-[calc(50%-1rem)] h-[calc(var(--section-height)*0.7)] grid-flow-col gap-4">
-            <Card v-for="offer in offers" :image="offer.fields.image" />
-          </div>
-        </div>
-      </div>
-    </div>
+  <main class="content-grid">
+    <Navigation v-if="!isAnimationAnywhere" :sections="sections" />
+    <template v-for="(section, index) in sections" :key="section._id">
+      <template v-if="section._stem.indexOf('witamy') > -1">
+        <Witamy class="full-width" :id="useGenHumanReadableId(section._stem)" v-bind="section.fields"
+          :link="useGenHumanReadableId(sections[index + 1]._stem)" />
+      </template>
+      <template v-else>
+        <GenerycznaSekcja class="full-width"
+          :class="{ 'mb-[--menu-height]': index + 1 === lengthOfSections && !isAnimationAnywhere }"
+          :id="useGenHumanReadableId(section._stem)" :isAnimation="isAnimation[index]" v-bind="section.fields"
+          @preview-clicked="previewClicked(index)" />
+      </template>
+
+      <!-- <ContentRendererMarkdown :value="section.body" /> -->
+    </template>
   </main>
 </template>
 
 <script setup lang="ts">
-import emblaCarouselVue from 'embla-carousel-vue'
+import { useGenHumanReadableId } from '../composable/useGenHumanReadableId'
 
-const [emblaRef] = emblaCarouselVue()
-
-const { data: offers } = await useAsyncData('offers', async () => {
-  return await queryContent('offers').find()
+const { data: sections } = await useAsyncData('sections', async () => {
+  return await queryContent().find()
 })
-console.log("🚀 ~ const{data:sections}=awaituseAsyncData ~ sections:", offers.value)
 
+const dataForMenu = sections.value.map((section) => {
+  return {
+    id: useGenHumanReadableId(section._stem),
+    title: section.fields.title,
+  }
+})
+const lengthOfSections = computed(() => toRaw(sections.value).length).value
+
+const isAnimation = ref([])
+isAnimation.value = Array(lengthOfSections).fill(false)
+const previewClicked = (index: number) => {
+  isAnimation.value = isAnimation.value.map((elem, i) => index === i ? !elem : elem)
+}
+const isAnimationAnywhere = ref(false);
+
+watch(isAnimation, (newIsAnimation, oldIsAnimation) => {
+  isAnimationAnywhere.value = newIsAnimation.some((elem) => elem)
+})
 </script>
+
+<style lang="css">
+.cos {}
+</style>
