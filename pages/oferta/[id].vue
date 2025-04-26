679810eedb0fa8be7410fc17e37b0390726fae45
mrGrochowski <mateusz.grochowski1992@gmail.com>
2025-04-26 15:23:05 +0200

    add slug

---
+++ pages/oferta/[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="content-grid">
+    <Navigation :sections="menuSections" />
+    <GenericOffer class="full-width"
+      v-bind="section.fields"
+     />
+
+  </main>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+
+const { data: section } = await useAsyncData('offerpreview', async () => {
+  return await queryContent('offers').where({ '_stem': { $contains: route.params.id } }).findOne()
+})
+console.log("🚀 ~ const{data:section}=awaituseAsyncData ~ section:", section)
+const menuSections = ref([
+  { id: 'witamy', title: 'Witamy' },
+  { id: 'oferta', title: 'Oferta' },
+  { id: 'media', title: 'Media społecznościowe' },
+  { id: 'kontakt', title: 'Kontakt' },
+])
+
+</script>
